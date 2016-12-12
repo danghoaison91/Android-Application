814555e935448cb66286b7c392ad2c5a759fd6eb
danghoaison91 <danghoaison91@gmail.com>
2016-12-12 02:16:00 +0000

    Adding checkbox cream

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -4,7 +4,9 @@ package com.example.android.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -12,10 +14,16 @@ import java.text.NumberFormat;
 /**
  * This app displays an order form to order coffee.
  */
+
 public class MainActivity extends AppCompatActivity {
 
-    int numberOfCoffes = 2;
-    int quantity = 5;
+    int numberOfCoffes = 0;
+    int coffePrice = 5;
+    int totalPrice = 0;
+    boolean cream = false;
+    final static int HAVE_CREAM = 5;
+    final static int NO_CREAM = 0;
+    int creamPrice = NO_CREAM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +31,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
 
-        displayPrice(numberOfCoffes * quantity);
+        displayPrice();
+
     }
 
     /**
@@ -55,6 +65,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * This method is called to check whether or not put in Cream
+     */
+    public boolean creamCheck(View v){
+
+        cream = ((CheckBox)v).isChecked();
+        if (cream){
+            creamPrice = HAVE_CREAM;
+        }else{
+            creamPrice = NO_CREAM;
+        }
+        return cream;
+    }
+    /**
+     * This method is used to calculate the price of order
+     */
+
+    private int calculatePrice(int quantity, int price, int creamPrice) {
+
+        return totalPrice = totalPrice + quantity * (price+creamPrice);
+
+    }
+
+    /**
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
@@ -65,9 +98,24 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number) {
-        String message = "Total: ";
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message +  NumberFormat.getCurrencyInstance().format(number) + "\n Thank you!");
+    private void displayPrice() {
+
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(toString());
+    }
+
+    @Override
+
+
+    public String toString() {
+
+        String message = "";
+        message = "Name: Dang Hoai Son\n";
+        message = message + "Add Cream: " + cream;
+        message = message + "\nQuantity: " + numberOfCoffes + "\n";
+        message = message + "Total: $" + calculatePrice(numberOfCoffes, coffePrice,creamPrice);
+        Log.e("MainActivity", "Cream Price is " + creamPrice );
+        message = message + "\nThank you!";
+        return message;
     }
 }
